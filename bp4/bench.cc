48ef9bbb364247acf239e9ee273b78eb973e38e0
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2018-05-13 09:16:47 +0200

    Use GLL quadrature for diagonal in BP3/BP4.

--- bp4/bench.cc
+++ bp4/bench.cc
@@ -59,6 +59,22 @@ void test(const unsigned int s,
                                            constraints);
   constraints.close();
   std_cxx11::shared_ptr<MatrixFree<dim,double> > matrix_free(new MatrixFree<dim,double>());
+
+  // create preconditioner based on the diagonal of the GLL quadrature with
+  // fe_degree+1 points
+  DiagonalMatrixBlocked<dim,double> diag_mat;
+  {
+    matrix_free->reinit(dof_handler, constraints, QGaussLobatto<1>(fe_degree+1),
+                        typename MatrixFree<dim,double>::AdditionalData());
+
+    Poisson::LaplaceOperator<dim,fe_degree,fe_degree+1,1,double,
+                             LinearAlgebra::distributed::Vector<double> > laplace_operator;
+    laplace_operator.initialize(matrix_free);
+
+    diag_mat.diagonal = laplace_operator.compute_inverse_diagonal();
+  }
+
+  // now go back to the actual operator with n_q_points points
   matrix_free->reinit(dof_handler, constraints, QGauss<1>(n_q_points),
                       typename MatrixFree<dim,double>::AdditionalData());
 
@@ -77,9 +93,6 @@ void test(const unsigned int s,
     for (unsigned int i=0; i<input.block(0).local_size(); ++i)
       if (!constraints.is_constrained(input.block(0).get_partitioner()->local_to_global(i)))
         input.block(d).local_element(i) = (i+d)%8;
-
-  DiagonalMatrixBlocked<dim,double> diag_mat;
-  diag_mat.diagonal = laplace_operator.compute_inverse_diagonal();
 
   Utilities::MPI::MinMaxAvg data =
     Utilities::MPI::min_max_avg(time.wall_time(), MPI_COMM_WORLD);
