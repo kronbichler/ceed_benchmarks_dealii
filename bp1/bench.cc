2260faf3444b007dabbe1f936d4dbffe0ab78ce8
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2019-02-18 15:41:34 +0100

    Add likwid annotations and some renumbering tests.

--- bp1/bench.cc
+++ bp1/bench.cc
@@ -1,6 +1,7 @@
 
 #include <deal.II/base/timer.h>
 #include <deal.II/distributed/tria.h>
+#include <deal.II/dofs/dof_tools.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/manifold.h>
@@ -15,6 +16,11 @@
 #include "../common_code/curved_manifold.h"
 #include "../common_code/mass_operator.h"
 #include "../common_code/solver_cg_optimized.h"
+
+
+#ifdef LIKWID_PERFMON
+#include <likwid.h>
+#endif
 
 using namespace dealii;
 
@@ -46,11 +52,13 @@ void test(const unsigned int s,
   for (unsigned int d=0; d<remainder; ++d)
     subdivisions[d] = 2;
   GridGenerator::subdivided_hyper_rectangle(tria, subdivisions, Point<dim>(), p2);
+
   GridTools::transform(std::bind(&MyManifold<dim>::push_forward, manifold,
                                  std::placeholders::_1),
                        tria);
   tria.set_all_manifold_ids(1);
   tria.set_manifold(1, manifold);
+
   tria.refine_global(n_refine);
 
   FE_Q<dim> fe(fe_degree);
@@ -60,9 +68,122 @@ void test(const unsigned int s,
 
   AffineConstraints<double> constraints;
   constraints.close();
+  typename MatrixFree<dim,double>::AdditionalData mf_data;
+  mf_data.initialize_mapping = false;
   std::shared_ptr<MatrixFree<dim,double> > matrix_free(new MatrixFree<dim,double>());
   matrix_free->reinit(mapping, dof_handler, constraints, QGauss<1>(n_q_points),
-                      typename MatrixFree<dim,double>::AdditionalData());
+                      mf_data);
+
+  // renumber Dofs to minimize the number of partitions in import indices of parititoner
+#if 0
+  std::vector<std::vector<unsigned int>> processors_involved(dof_handler.locally_owned_dofs().n_elements());
+  std::vector<types::global_dof_index> dof_indices(fe.dofs_per_cell);
+  for (auto &cell : dof_handler.active_cell_iterators())
+    if (cell->is_ghost())
+      {
+        cell->get_dof_indices(dof_indices);
+        for (auto i : dof_indices)
+          {
+            if (dof_handler.locally_owned_dofs().is_element(i))
+              processors_involved[dof_handler.locally_owned_dofs().index_within_set(i)]
+                .push_back(cell->subdomain_id());
+          }
+      }
+  // sort & compress out duplicates
+  for (std::vector<unsigned int> &v : processors_involved)
+    if (v.size() > 1)
+      {
+        std::sort(v.begin(), v.end());
+        v.erase(std::unique(v.begin(), v.end()), v.end());
+      }
+  std::map<std::vector<unsigned int>, std::vector<unsigned int>,
+           std::function<bool(const std::vector<unsigned int> &,
+                              const std::vector<unsigned int> &)>>
+    sorted_entries{[](const std::vector<unsigned int> &a,
+                      const std::vector<unsigned int> &b)
+      {
+        if (a.size() < b.size())
+          return true;
+        if (a.size() == b.size())
+          {
+            for (unsigned int i=0; i<a.size(); ++i)
+              if (a[i] < b[i])
+                return true;
+              else if (a[i] > b[i])
+                return false;
+          }
+        return false;
+      }
+  };
+  for (unsigned int i=0; i<processors_involved.size(); ++i)
+    {
+      std::vector<unsigned int> &v = sorted_entries[processors_involved[i]];
+      v.push_back(i);
+    }
+
+  std::vector<unsigned char> touch_count(dof_handler.locally_owned_dofs().n_elements());
+  std::vector<unsigned int> local_dofs_mf;
+  for (unsigned int cell=0; cell<matrix_free->n_cell_batches(); ++cell)
+    {
+      matrix_free->get_dof_info().get_dof_indices_on_cell_batch(local_dofs_mf, cell, true);
+      std::sort(local_dofs_mf.begin(), local_dofs_mf.end());
+      local_dofs_mf.erase(std::unique(local_dofs_mf.begin(), local_dofs_mf.end()),
+                          local_dofs_mf.end());
+      for (unsigned int i : local_dofs_mf)
+        if (i<dof_handler.locally_owned_dofs().n_elements())
+          touch_count[i]++;
+    }
+
+  std::vector<unsigned int> new_numbers;
+  new_numbers.reserve(dof_handler.locally_owned_dofs().n_elements());
+  for (auto i : sorted_entries[std::vector<unsigned int>()])
+    if (touch_count[i] == 0)
+      new_numbers.push_back(i);
+  for (auto i : sorted_entries[std::vector<unsigned int>()])
+    if (touch_count[i] == 1)
+      new_numbers.push_back(i);
+  const unsigned int single_size = new_numbers.size();
+  for (auto i : sorted_entries[std::vector<unsigned int>()])
+    if (touch_count[i] > 1)
+      new_numbers.push_back(i);
+  const unsigned int multiple_size = new_numbers.size() - single_size;
+  sorted_entries.erase(std::vector<unsigned int>());
+
+  for (auto &it : sorted_entries)
+    for (auto i : it.second)
+      new_numbers.push_back(i);
+  const unsigned int multiproc_size = new_numbers.size() - single_size - multiple_size;
+
+  std::cout << "P" << Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) << " "
+            << single_size << " " << multiple_size << " " << multiproc_size << std::endl;
+
+  AssertThrow(new_numbers.size() == dof_handler.locally_owned_dofs().n_elements(),
+              ExcMessage("Dimension mismatch " + std::to_string(new_numbers.size()) +
+                         " vs " + std::to_string(dof_handler.locally_owned_dofs().n_elements())));
+
+  std::vector<types::global_dof_index> new_global_numbers(dof_handler.locally_owned_dofs().n_elements());
+  for (unsigned int i=0; i<new_numbers.size(); ++i)
+    new_global_numbers[new_numbers[i]] = dof_handler.locally_owned_dofs().nth_index_in_set(i);
+
+  dof_handler.renumber_dofs(new_global_numbers);
+#endif
+
+  if (false)
+  {
+    IndexSet locally_active_dofs;
+    DoFTools::extract_locally_active_dofs(dof_handler, locally_active_dofs);
+    Utilities::MPI::Partitioner partitioner;
+    partitioner.reinit(dof_handler.locally_owned_dofs(), locally_active_dofs,
+                       tria.get_communicator());
+    std::cout << Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)
+              << " n_import_indices " << partitioner.n_import_indices()
+              << " import_indices.size() " << partitioner.import_indices().size()
+              << std::endl;
+  }
+
+  mf_data.initialize_mapping = true;
+  matrix_free->reinit(mapping, dof_handler, constraints, QGauss<1>(n_q_points),
+                      mf_data);
 
   Mass::MassOperator<dim,fe_degree,n_q_points> mass_operator;
   mass_operator.initialize(matrix_free);
@@ -98,6 +219,9 @@ void test(const unsigned int s,
         std::cout << "Norm of diagonal for preconditioner: " << diag_norm << std::endl;
     }
 
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_START("cg_solver");
+#endif
   double solver_time = 1e10;
   for (unsigned int t=0; t<4; ++t)
     {
@@ -115,6 +239,9 @@ void test(const unsigned int s,
       data = Utilities::MPI::min_max_avg(time.wall_time(), MPI_COMM_WORLD);
       solver_time = std::min(data.max, solver_time);
     }
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_STOP("cg_solver");
+#endif
 
   if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0 &&
       short_output==false)
@@ -132,6 +259,9 @@ void test(const unsigned int s,
                 << std::endl;
     }
 
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_START("cg_solver_opt");
+#endif
   SolverCGOptimized<LinearAlgebra::distributed::Vector<double> > solver2(solver_control);
   double solver_time2 = 1e10;
   for (unsigned int t=0; t<4; ++t)
@@ -150,10 +280,16 @@ void test(const unsigned int s,
       data = Utilities::MPI::min_max_avg(time.wall_time(), MPI_COMM_WORLD);
       solver_time2 = std::min(data.max, solver_time2);
     }
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_STOP("cg_solver_opt");
+#endif
   const unsigned int solver_its2 = solver_control.last_step();
 
   SolverCGOptimizedAllreduce<LinearAlgebra::distributed::Vector<double> > solver3(solver_control);
   double solver_time3 = 1e10;
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_START("cg_solver_optv");
+#endif
   for (unsigned int t=0; t<4; ++t)
     {
       output = 0;
@@ -170,9 +306,15 @@ void test(const unsigned int s,
       data = Utilities::MPI::min_max_avg(time.wall_time(), MPI_COMM_WORLD);
       solver_time3 = std::min(data.max, solver_time3);
     }
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_STOP("cg_solver_optv");
+#endif
 
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_START("matvec");
+#endif
   double matvec_time = 1e10;
-  for (unsigned int t=0; t<2; ++t)
+  for (unsigned int t=0; t<4; ++t)
     {
       time.restart();
       for (unsigned int i=0; i<50; ++i)
@@ -180,6 +322,9 @@ void test(const unsigned int s,
       data = Utilities::MPI::min_max_avg(time.wall_time(), MPI_COMM_WORLD);
       matvec_time = std::min(data.max/50, matvec_time);
     }
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_STOP("matvec");
+#endif
 
   if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
     std::cout << std::setw(2) << fe_degree << " | " << std::setw(2) << n_q_points
@@ -226,6 +371,10 @@ void do_test(const int s_in,
 
 int main(int argc, char** argv)
 {
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_INIT;
+#endif
+
   Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
 
   unsigned int degree = 1;
@@ -272,6 +421,10 @@ int main(int argc, char** argv)
     do_test<3,16,18>(s, compact_output);
   else
     AssertThrow(false, ExcMessage("Only degrees up to 16 implemented"));
+
+#ifdef LIKWID_PERFMON
+  LIKWID_MARKER_CLOSE;
+#endif
 
   return 0;
 }
